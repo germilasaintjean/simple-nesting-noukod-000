2ef74323650500adb5d1e5f8256f49d153efdb65
Saint-Jean Germila <germila@noukod.com>
2018-10-02 19:20:26 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -148,7 +148,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages] << "Milie"
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
   programmer_hash
 
 end
